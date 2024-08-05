b8cc3015b26a5ee23f939a4b91c726144733908b
Huseyn <59554521+huseynyusif@users.noreply.github.com>
2024-08-05 21:30:19 +0400

    added VenueMapper

--- src/main/java/az/example/eventsapp/mapper/VenueMapper.java
+++ src/main/java/az/example/eventsapp/mapper/VenueMapper.java
@@ -1,13 +1,12 @@
 package az.example.eventsapp.mapper;
 
 import az.example.eventsapp.entity.VenueEntity;
+import az.example.eventsapp.request.VenueRequest;
 import az.example.eventsapp.response.VenueResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, unmappedSourcePolicy = ReportingPolicy.IGNORE)
 public interface VenueMapper {
-    VenueResponse toVenueResponse(VenueEntity venue);
-
-    VenueEntity toVenueEntity(VenueResponse venueResponse);
+    VenueEntity toVenueEntity(VenueRequest venueRequest);
 }
